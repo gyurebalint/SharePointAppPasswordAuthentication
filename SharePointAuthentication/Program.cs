a225f1d44e2367eb4edd91194720cf7ada1890f6
gyurebalint@gmail.com <gyurebalint@gmail.com>
2020-10-18 13:44:05 +0200

    first commit

---
+++ SharePointAuthentication/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.SharePoint.Client;
+using OfficeDevPnP.Core.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharePointAuthentication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cred = CredentialManager.GetCredential("DrawingRegistryAccess");
+            using (ClientContext context = new ClientContext("Your SharePoint site's URL here").GetAppPasswordAuthenticatedContext("Your UserName", "Your Password")) 
+            {
+                
+                context.Load(context.Web);
+
+                context.ExecuteQuery();
+
+                Console.WriteLine("Name of the web is: " + context.Web.Title);
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ SharePointAuthentication/SharePointConnection.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.SharePoint.Client;
+using OfficeDevPnP.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharePointAuthentication
+{
+
+    public static class SharePointConnection
+    {
+        public static ClientContext GetAppPasswordAuthenticatedContext(this ClientContext clientContext, string userName, string password)
+        {
+            MsOnlineClaimsHelper claims = new MsOnlineClaimsHelper(clientContext.Url, userName, password);
+            ClientContext Context = new ClientContext(clientContext.Url);
+            Context.ExecutingWebRequest += claims.clientContext_ExecutingWebRequest;
+
+            return Context;
+        }
+    }
+}
